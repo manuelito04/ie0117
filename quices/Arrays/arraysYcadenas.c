4d29e6715dd7d1c13dd0df566620b000e0c0aa64
manuelito04 <MANUEL.OTAROLA@ucr.ac.cr>
2024-09-23 19:37:06 -0600

    Agregando archivo arraysYcadenas.c correspondiente al quiz del 23/09

---
+++ quices/Arrays/arraysYcadenas.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // Array de 20 caracteres para establecer el maximo.
+    char palabra[20];
+
+    // Ingresar una palabra:
+    printf("Ingrese una palabra de maximo 20 letras: ");
+
+    //Leer hasta el máximo de 20 caracteres:
+    scanf("%20s", palabra);
+
+    // Imprimir palabra ingresada:
+    printf("La palabra ingresada es: %s\n", palabra);
+
+    // Longitud de la palabra:
+    int longitud = strlen(palabra);
+    printf("La longitud de la palabra es: %d\n", longitud);
+
+    // Invertir palabra ingresada:
+    printf("La palabra tras invertirla es: ");
+    for (int i = longitud - 1; i>=0; i--){
+        putchar(palabra[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
