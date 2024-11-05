c16a4c0657bac0b9999dafcdc89d7dddf1258c5a
manuelito04 <MANUEL.OTAROLA@ucr.ac.cr>
2024-11-04 18:28:28 -0600

    Subiendo archivo de quiz de enums

---
+++ quices/enums/enums.c
@@ -0,0 +1,43 @@
+// El presente programa crea un enum de niveles de log e implementa una funcion para imprimir mensajes 
+// segun el nivel.
+
+// Realizado por Manuel Alejandro Otárola Madrigal - C05740.
+
+#include <stdio.h>
+
+// Se define el enum para los niveles de log.
+enum LogNiveles {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+};
+
+// Funcion para imprimir mensajes segun el nivel de log.
+void logMensaje(enum LogNiveles nivel, const char *mensaje) {
+    switch (nivel) {
+        case DEBUG:
+            printf("[DEBUG]: %s\n", mensaje);
+            break;
+
+        case INFO:
+            printf("[INFO]: %s\n", mensaje);
+            break;
+
+        case WARN:
+            printf("[WARN]: %s\n", mensaje);
+            break;
+        case ERROR:
+            printf("[ERROR]: %s\n", mensaje);
+            break;
+    }
+}
+
+int main() {
+    logMensaje(INFO, "Inicio del programa");
+    logMensaje(DEBUG, "Variable x inicializada a 0");
+    logMensaje(WARN, "La conexion es inestable");
+    logMensaje(ERROR, "Archivo no encontrado");
+
+    return 0;
+}
